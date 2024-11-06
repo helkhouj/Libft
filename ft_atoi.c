351e8b3d729c22445d03f4c383f243a0a49df11b
helkhouj <hichamelkhouji@gmail.com>
2024-11-06 15:21:17 +0100

    Conversion Functions

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: helkhouj <helkhouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:18:04 by helkhouj          #+#    #+#             */
-/*   Updated: 2024/11/06 13:32:35 by helkhouj         ###   ########.fr       */
+/*   Updated: 2024/11/06 15:19:18 by helkhouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,24 @@
 
 int		ft_atoi(const char *str)
 {
+    size_t i = 0;
+    int sign = 1;
+    int result = 0; 
 
+    while(str[i] == ' ' || str[i] == '\v' || str[i] == '\t' || str[i] == '\r' || str[i] == '\n' || str[i] == '\f')
+        i++;
+
+    if(str[i] == '-' || str[i] == '+')
+    {
+        if(str[i] == '-')
+            sign = -1;
+        i++;
+    }
+
+    while(str[i] >= '0' && str[i] <= '9')
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    return (result * sign);
 }
