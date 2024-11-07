6f40bc163e53ec6d118d8bca1d712615a3482c57
helkhouj <hichamelkhouji@gmail.com>
2024-11-07 10:10:17 +0100

    Conversion Functions

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,32 +6,34 @@
 /*   By: helkhouj <helkhouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:18:04 by helkhouj          #+#    #+#             */
-/*   Updated: 2024/11/06 15:19:18 by helkhouj         ###   ########.fr       */
+/*   Updated: 2024/11/07 10:08:13 by helkhouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    size_t i = 0;
-    int sign = 1;
-    int result = 0; 
+	size_t	i;
+	int		sign;
+	int		result;
 
-    while(str[i] == ' ' || str[i] == '\v' || str[i] == '\t' || str[i] == '\r' || str[i] == '\n' || str[i] == '\f')
-        i++;
-
-    if(str[i] == '-' || str[i] == '+')
-    {
-        if(str[i] == '-')
-            sign = -1;
-        i++;
-    }
-
-    while(str[i] >= '0' && str[i] <= '9')
-    {
-        result = result * 10 + (str[i] - '0');
-        i++;
-    }
-    return (result * sign);
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\v' || str[i] == '\t' || str[i] == '\r'
+		|| str[i] == '\n' || str[i] == '\f')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return (result * sign);
 }
