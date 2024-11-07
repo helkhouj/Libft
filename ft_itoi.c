cec44a52cc5e6e15bf90bde3aea14a0579058272
helkhouj <hichamelkhouji@gmail.com>
2024-11-07 10:53:10 +0100

    Conversion Functions

--- ft_itoi.c
+++ ft_itoi.c
@@ -12,7 +12,35 @@
 
 #include "libft.h"
 
-char	*ft_itoa(int n)
+char *ft_itoa(int n)
 {
-    
+    int len = get_num_length(n);
+    int sign = 1;
+    char *str;
+
+    if (n < 0)
+        sign = -1;
+
+    str = (char *)malloc(sizeof(char) * (len + 1));
+    if (!str)
+        return NULL;
+
+    str[len] = '\0';
+    if (n == 0)
+    {
+        str[0] = '0';
+        return str;
+    }
+
+    while (n != 0)
+    {
+        str[--len] = (sign * (n % 10)) + '0';
+        n /= 10;
+    }
+
+    if (sign == -1)
+        str[0] = '-';
+
+    return str;
 }
+
