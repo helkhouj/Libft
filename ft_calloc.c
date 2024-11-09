a9a4a68f53de85db43f7b3694c8b01210a3560e1
helkhouj <hichamelkhouji@gmail.com>
2024-11-09 14:37:01 +0100

    Allocation Functions

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: helkhouj <helkhouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:29:49 by helkhouj          #+#    #+#             */
-/*   Updated: 2024/11/06 13:32:35 by helkhouj         ###   ########.fr       */
+/*   Updated: 2024/11/09 14:31:29 by helkhouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,23 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-    void *ptr;
-    size_t total_size = count * size;
-    size_t i = 0;
+	void			*ptr;
+	size_t			total_size;
+	size_t			i;
+	unsigned char	*p;
 
-    if(cout != 0 && (total_size / count) != size)
-        return(NULL);
-
-    ptr = malloc(total_size);
-    if(!ptr)
-        return(NULL);
-
-    unsigned char *p = (unsigned char *)ptr;
-    while(i < total_size)
-    {
-        ptr[i] = 0;
-        i++;
-    }
-    return ptr;
+	total_size = count * size;
+	i = 0;
+	if (cout != 0 && (total_size / count) != size)
+		return (NULL);
+	ptr = malloc(total_size);
+	if (!ptr)
+		return (NULL);
+	p = (unsigned char *)ptr;
+	while (i < total_size)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	return (ptr);
 }
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: helkhouj <helkhouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:36:30 by helkhouj          #+#    #+#             */
-/*   Updated: 2024/11/06 13:36:52 by helkhouj         ###   ########.fr       */
+/*   Updated: 2024/11/09 14:35:35 by helkhouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,22 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-    char *sub_str;
-    size_t str_len;
-    size_t actual_len;
+	char	*sub_str;
+	size_t	str_len;
+	size_t	actual_len;
 
-    if(!s)
-        return(NULL);
-
-    str_len = ft_strlen(s);
-
-    if(start >= str_len)
-        return (char *)malloc(1);
-
-    actual_len = str_len - start;
-
-    if(actual_len > len)
-        actual_len = len;
-
-    sub_str = (char *)malloc(actual_len + 1);
-    if(!sub_str)
-        return(NULL);
-    
-    ft_memcpy(sub_str, s + start, actual_len);
-    sub_str[actual_len] = '\0';
-    return sub_str;
+	if (!s)
+		return (NULL);
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+		return ((char *)malloc(1));
+	actual_len = str_len - start;
+	if (actual_len > len)
+		actual_len = len;
+	sub_str = (char *)malloc(actual_len + 1);
+	if (!sub_str)
+		return (NULL);
+	ft_memcpy(sub_str, s + start, actual_len);
+	sub_str[actual_len] = '\0';
+	return (sub_str);
 }
