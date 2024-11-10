eec176c527829bdab396d752c3c84e782ead6e7b
helkhouj <hichamelkhouji@gmail.com>
2024-11-10 16:58:48 +0100

    Memory Functions

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: helkhouj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: helkhouj <helkhouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 12:48:47 by helkhouj          #+#    #+#             */
-/*   Updated: 2024/11/06 12:49:05 by helkhouj         ###   ########.fr       */
+/*   Updated: 2024/11/10 13:43:36 by helkhouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t			i;
-	unsigned char	*d;
-	unsigned char	*s;
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
 
 	i = 0;
 	d = (unsigned char *)dst;
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: helkhouj <helkhouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:34:45 by helkhouj          #+#    #+#             */
-/*   Updated: 2024/11/06 13:35:10 by helkhouj         ###   ########.fr       */
+/*   Updated: 2024/11/10 16:56:39 by helkhouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-    
+	char	*joined_str;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	total_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	total_len = s1_len + s2_len;
+	joined_str = (char *)malloc(total_len + 1);
+	if (joined_str)
+	{
+		ft_memcpy(joined_str, s1, s1_len);
+		ft_memcpy(joined_str + s1_len, s2, s2_len);
+		joined_str[total_len] = '\0';
+	}
+	return (joined_str);
 }
