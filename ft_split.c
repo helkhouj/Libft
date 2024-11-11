acdfb9c5803f9601930695313859618e25e7d123
helkhouj <hichamelkhouji@gmail.com>
2024-11-11 18:55:45 +0100

    Allocation Functions

--- ft_split.c
+++ ft_split.c
@@ -12,71 +12,80 @@
 
 #include "libft.h"
 
-static size_t count_words(const char *s, char c)
+static size_t	count_words(const char *s, char c)
 {
-    size_t count = 0;
-    int in_word = 0;
+	size_t	count;
+	int		in_word;
 
-    while (*s)
-    {
-        if(*s != c && !in_word)
-        {
-            in_word = 1;
-            count++;
-        }
-        else if (*s == c)
-            in_word = 0;
-        s++;
-    }
-    return (count);
+	count = 0;
+	in_word = 0;
+	while (*s)
+	{
+		if (*s != c && !in_word)
+		{
+			in_word = 1;
+			count++;
+		}
+		else if (*s == c)
+			in_word = 0;
+		s++;
+	}
+	return (count);
 }
 
-static char *word_dup(const char *s, size_t start, size_t len)
+static char	*word_dup(const char *s, size_t start, size_t len)
 {
-    char *word = (char *)malloc((len + 1) * sizeof(char));
-    size_t i = 0;
-    if(!word)
-        return(NULL);
-    while (i < len)
-    {
-        word[i] = s[start + i];
-        i++;
-    }
-    word[len] = '\0';
-    return word;
+	char	*word;
+	size_t	i;
+
+	word = (char *)malloc((len + 1) * sizeof(char));
+	i = 0;
+	if (!word)
+		return (NULL);
+	while (i < len)
+	{
+		word[i] = s[start + i];
+		i++;
+	}
+	word[len] = '\0';
+	return (word);
 }
 
-static void free_split(char **result, size_t index)
+static void	free_split(char **result, size_t index)
 {
-    while(index > 0)
-        free(result[--index]);
-    free(result);
+	while (index > 0)
+		free(result[--index]);
+	free(result);
 }
 
 char	**ft_split(char const *s, char c)
 {
-    char **result;
-    size_t i = 0;
-    size_t words_index = 0;
-    size_t word_count;
-    size_t word_start = 0;
-    size_t word_len = 0;
+	char	**result;
+	size_t	i;
+	size_t	words_index;
+	size_t	word_count;
+	size_t	word_start;
+	size_t	word_len;
 
-    if (!s || !(result = malloc((count_words(s, c) + 1) * sizeof(char *))))
-        return NULL;
-    word_count = count_words(s, c);
-    while (s[i])
-    {
-        if (s[i] != c && word_len++ == 0)
-            word_start = i;
-        else if ((s[i] == c || s[i + 1] == '\0') && word_len)
-        {
-            if (!(result[word_index++] = word_dup(s, word_start, word_len)))
-                return (free_split(result, word_index - 1), NULL);
-            word_len = 0;
-        }
-        i++;
-    }
-    result[word_index] = NULL;
-    return result;
+	i = 0;
+	words_index = 0;
+	word_start = 0;
+	word_len = 0;
+	if (!s || !(result = malloc((count_words(s, c) + 1) * sizeof(char *))))
+		return (NULL);
+	word_count = count_words(s, c);
+	while (s[i])
+	{
+		if (s[i] != c && word_len++ == 0)
+			word_start = i;
+		else if ((s[i] == c || s[i + 1] == '\0') && word_len)
+		{
+			if (!(result[word_index++] = word_dup(s, word_start, word_len)))
+				return (free_split(result, word_index - 1), NULL);
+			word_len = 0;
+		}
+		i++;
+	}
+	result[word_index] = NULL;
+	return (result);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,40 +6,40 @@
 /*   By: helkhouj <helkhouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:35:56 by helkhouj          #+#    #+#             */
-/*   Updated: 2024/11/06 13:36:19 by helkhouj         ###   ########.fr       */
+/*   Updated: 2024/11/11 18:51:39 by helkhouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
-int is_in_set(char c, const char *set)
+int	is_in_set(char c, const char *set)
 {
-    while (*set)
-    {
-        if (c == *set)
-            return (1);
-        set++
-    }
-    return (0);
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-    size_t start = 0;
-    size_t end;
-    char *trimmed_str;
+	size_t	start;
+	size_t	end;
+	char	*trimmed_str;
 
-    if (!s1 || !set)
-        return (NULL);
-    while (s1[start] && is_in_set(s1[start], set))
-        start++;
-    while (end > start && is_in_set(s1[end - 1]), set)
-        end--;
-    trimmed_str = (char *)malloc((end - start + 1) * sizeof(char));
-    if (!trimmed_str)
-        return (NULL);
-    ft_memcpy(trimmed_str, s1 + start, end - start);
-    trimmed_str[end - start] = '\0';
-    return(trimmed_str);
+	start = 0;
+	if (!s1 || !set)
+		return (NULL);
+	while (s1[start] && is_in_set(s1[start], set))
+		start++;
+	while (end > start && is_in_set(s1[end - 1]), set)
+		end--;
+	trimmed_str = (char *)malloc((end - start + 1) * sizeof(char));
+	if (!trimmed_str)
+		return (NULL);
+	ft_memcpy(trimmed_str, s1 + start, end - start);
+	trimmed_str[end - start] = '\0';
+	return (trimmed_str);
 }
