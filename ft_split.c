a1e379547b5f90368b5a95d3b6fc7c9ee93b3890
helkhouj <hichamelkhouji@gmail.com>
2024-11-11 18:23:11 +0100

    Allocation Memory

--- ft_split.c
+++ ft_split.c
@@ -12,6 +12,47 @@
 
 #include "libft.h"
 
+static size_t count_words(const char *s, char c)
+{
+    size_t count = 0;
+    int in_word = 0;
+
+    while (*s)
+    {
+        if(*s != c && !in_word)
+        {
+            in_word = 1;
+            count++;
+        }
+        else if (*s == c)
+            in_word = 0;
+        s++;
+    }
+    return (count);
+}
+
+static char *word_dup(const char *s, size_t start, size_t len)
+{
+    char *word = (char *)malloc((len + 1) * sizeof(char));
+    size_t i = 0;
+    if(!word)
+        return(NULL);
+    while (i < len)
+    {
+        word[i] = s[start + i];
+        i++;
+    }
+    word[len] = '\0';
+    return word;
+}
+
+static void free_split(char **result, size_t index)
+{
+    while(index > 0)
+        free(result[--index]);
+    free(result);
+}
+
 char	**ft_split(char const *s, char c)
 {
     
