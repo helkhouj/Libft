e5a915478869c4c178a27bdb0763f063bb59d13f
helkhouj <hichamelkhouji@gmail.com>
2024-11-18 13:45:45 +0100

    Linked List Functions

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -10,3 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void ft_lstclear(t_list **lst, void (*del)(void *))
+{
+    t_list *temp;
+
+    if (!lst || !del)
+        return;
+
+    while (*lst)
+    {
+        temp = (*lst)->next;  
+        del((*lst)->content); 
+        free(*lst);           
+        *lst = temp;       
+    }
+    *lst = NULL;              
+}
