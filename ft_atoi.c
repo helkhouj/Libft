4237947f728cdd86994e939707c666e0a56fedbf
helkhouj <hichamelkhouji@gmail.com>
2024-11-22 16:13:10 +0100

    Conversion functions

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,24 +6,45 @@
 /*   By: helkhouj <helkhouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 13:18:04 by helkhouj          #+#    #+#             */
-/*   Updated: 2024/11/07 10:08:13 by helkhouj         ###   ########.fr       */
+/*   Updated: 2024/11/22 16:11:54 by helkhouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
+
+static size_t	skip_whitespace(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\v' || str[i] == '\t' || str[i] == '\r'
+		|| str[i] == '\n' || str[i] == '\f')
+		i++;
+	return (i);
+}
+
+static int	check_overflow(int result, int digit, int sign)
+{
+	if (result > (INT_MAX - digit) / 10)
+	{
+		if (sign == 1)
+			return (INT_MAX);
+		return (INT_MIN);
+	}
+	return (0);
+}
 
 int	ft_atoi(const char *str)
 {
 	size_t	i;
 	int		sign;
 	int		result;
+	int		overflow;
 
-	i = 0;
+	i = skip_whitespace(str);
 	sign = 1;
 	result = 0;
-	while (str[i] == ' ' || str[i] == '\v' || str[i] == '\t' || str[i] == '\r'
-		|| str[i] == '\n' || str[i] == '\f')
-		i++;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
@@ -32,16 +53,11 @@ int	ft_atoi(const char *str)
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (result > (INT_MAX - (str[i] - '0')) / 10)
-		{
-			if(sign == 1)
-				return (INT_MAX);
-			else 
-				return (INT_MIN);
-		}
+		overflow = check_overflow(result, str[i] - '0', sign);
+		if (overflow != 0)
+			return (overflow);
 		result = result * 10 + (str[i] - '0');
 		i++;
 	}
 	return (result * sign);
 }
-
