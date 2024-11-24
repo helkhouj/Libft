ba877e0f1e7fc673f858299377097a03f6790590
helkhouj <hichamelkhouji@gmail.com>
2024-11-24 19:54:21 +0100

    nn

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,26 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: helkhouj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: helkhouj <helkhouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 09:42:30 by helkhouj          #+#    #+#             */
-/*   Updated: 2024/11/06 09:42:36 by helkhouj         ###   ########.fr       */
+/*   Updated: 2024/11/24 19:52:57 by helkhouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strchr(const char *s, int c) {
-    size_t i = 0;
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
 
-    while (s[i]) {
-        if (s[i] == (char)c)
-            return ((char *)&s[i]);
-        i++;
-    }
-
-    if ((char)c == '\0')
-        return ((char *)&s[i]);
-
-    return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if ((char)c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,27 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: helkhouj <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: helkhouj <helkhouj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 09:42:58 by helkhouj          #+#    #+#             */
-/*   Updated: 2024/11/06 09:43:01 by helkhouj         ###   ########.fr       */
+/*   Updated: 2024/11/24 19:53:10 by helkhouj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strrchr(const char *s, int c) {
-    size_t len = 0;
-    ssize_t i;
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	len;
+	ssize_t	i;
 
-    while (s[len])
-        len++;
-
-    i = len;
-    while (i >= 0) {
-        if (s[i] == (char)c)
-            return ((char *)&s[i]);
-        i--;
-    }
-    return (NULL);
+	len = 0;
+	while (s[len])
+		len++;
+	i = len;
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i--;
+	}
+	return (NULL);
 }
